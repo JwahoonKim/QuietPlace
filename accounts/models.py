2c34d0744f573318d043adc8b279d79490b5223c
jwahoonKim <72662822+JwahoonKim@users.noreply.github.com>
2021-07-14 03:18:35 +0900

    csrf_token 추가, Model 초안 작성

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,23 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)    
-    college = models.CharField(max_length=20, blank=True)    
-    major = models.CharField(max_length=20, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    nickname = models.CharField(max_length=20)
+    is_login = False
+
+    def __str__(self):
+        return f'id={self.id}, user_id={self.user.id}, nickname={self.nickname}'
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
