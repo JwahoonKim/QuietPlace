10ce43f029fd132ff1ee98038c23761f653b2c91
jiwoo <sdr06042@naver.com>
2021-07-15 18:28:31 +0900

    아까 진짜진짜

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('accounts/', accounts.views.signup, name='signup'),
     path('accounts/login/', accounts.views.login, name='login'),
     path('accounts/revise', accounts.views.revise, name='revise'),
-    path('accounts/my_page', accounts.views.my_page, name='my_page'),
 ]
