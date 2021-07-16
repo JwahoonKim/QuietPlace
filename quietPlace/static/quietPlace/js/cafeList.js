2ee97541c27f34de91d2085e32f15bbb4b671798
jwahoonkim <kjhoon0330@snu.ac.kr>
2021-07-16 23:07:41 +0900

    태그 적용시 Cafe 필터링 되는 기능

--- quietPlace/static/quietPlace/js/cafeList.js
+++ quietPlace/static/quietPlace/js/cafeList.js
@@ -1,3 +1,47 @@
+let filterCondition = [];
+
+const handleCafe = () => {
+    const selectedTag = [];
+    const getCafe = () => {
+        return [... document.querySelectorAll('.cafe-box')];
+    }
+
+    // console.log(getCafe()[0].getElementsByTagName('li'));
+    const filtering = () => {
+        filterCondition.forEach((tag_positive) => {
+            HideNotPositiveCafe(tag_positive);
+        })
+    }
+
+    const getTagList = (cafe) => {
+        return Array.from(cafe.getElementsByTagName('li'));   
+    }
+
+    const isTagPositive = (cafe, tag_positive) => {
+        cafeTagList = getTagList(cafe);
+        for(i = 0; i < cafeTagList.length; i++){
+            if (cafeTagList[i].classList.contains(tag_positive)) {
+                return true;
+            }
+            // 끝까지 다 봤는데 그 태그 positive가 아닌 경우
+            if (i === cafeTagList.length - 1){
+                return false;
+            }
+        }
+    }
+
+    const HideNotPositiveCafe = (tag_positive) => {
+        cafes = getCafe();
+        cafes.forEach( cafe => {
+            if (isTagPositive(cafe, tag_positive) === false){
+                cafe.classList.add('hide');
+            }
+        })
+    }
+
+    filtering();
+}
+
 const handleTag = () => {
     const getTags = () => {
         return [ ... document.querySelectorAll(".filter-tag-button")];
@@ -8,9 +52,13 @@ const handleTag = () => {
         tagDom.onclick = () => {
             if (tagDom.classList.contains('selected')) {
                 tagDom.classList.remove('selected');
+                filterCondition = filterCondition.filter(tag => tag !== `${tagDom.id}_positive`)
             }
             else {
                 tagDom.classList.add('selected');
+                filterCondition.push(`${tagDom.id}_positive`);
+                handleCafe();
+                console.log(filterCondition)
             }
         }
     }
@@ -19,12 +67,8 @@ const handleTag = () => {
     getTags().forEach(tagDom => {
         handleOnClick(tagDom);
     })
-
 }
 
-handleTag()
+handleTag();
 
-// const hadleCafe = () => {
-
-// }
 
