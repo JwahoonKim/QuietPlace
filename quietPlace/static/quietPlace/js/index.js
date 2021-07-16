b54f99b489df6133d6ff0cc3a8642a43dab5f841
jiwoo <sdr06042@naver.com>
2021-07-16 23:08:54 +0900

    mainpage 수정ing

--- quietPlace/static/quietPlace/js/index.js
+++ quietPlace/static/quietPlace/js/index.js
@@ -2,9 +2,23 @@ console.log('connect-complete')
 
 const searchCafe = (inputValue) => {
     
-}
+};
 
 // Cafe를 전부 가져오는 함수
 const getAllCafe = () => {
 
+};
+
+// 메인 이미지 슬라이드쇼
+var myImage=document.getElementById("mainImage");
+var imageArray=["forHeader/images/woman.jpg", "forHeader/images/coffee.jpg", "forHeader/images/computer.jpg", "forHeader/images/main-ppl.jpg"]
+var imageIndex=0;
+
+function changeImage () {
+    myImage.setAttribute("src", imageArray[imageIndex]);
+    imageIndex++;
+    if(imageIndex >= imageArray.length) {
+        imageIndex = 0;
+    }
 }
+setInterval(changeImage,3000);
