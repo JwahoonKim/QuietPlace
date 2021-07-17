e7b866c449d7b91d7feb265cfa003079c8926f23
jiwoo <sdr06042@naver.com>
2021-07-17 20:51:56 +0900

    mainpage ing...2

--- quietPlace/static/quietPlace/js/index.js
+++ quietPlace/static/quietPlace/js/index.js
@@ -8,17 +8,3 @@ const searchCafe = (inputValue) => {
 const getAllCafe = () => {
 
 };
-
-// 메인 이미지 슬라이드쇼
-var myImage=document.getElementById("mainImage");
-var imageArray=["forHeader/images/woman.jpg", "forHeader/images/coffee.jpg", "forHeader/images/computer.jpg", "forHeader/images/main-ppl.jpg"]
-var imageIndex=0;
-
-function changeImage () {
-    myImage.setAttribute("src", imageArray[imageIndex]);
-    imageIndex++;
-    if(imageIndex >= imageArray.length) {
-        imageIndex = 0;
-    }
-}
-setInterval(changeImage,3000);
